565bb8bb0fff8b5369246bb76e326bfb2c87921a
Suraj Mandal <surajmandalcell@users.noreply.github.com>
2017-11-05 23:40:37 +0530

    Program written

--- Assignment_1/WAP_to_find_the_area_of_a_triangle_using_formula_given[Prog 1].c
+++ Assignment_1/WAP_to_find_the_area_of_a_triangle_using_formula_given[Prog 1].c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-	return 0;
+  float a,b,c,s=0,area=0;
+  printf("Enter the sides of triangle\n");
+  printf("Enter side a : ");
+  scanf("%f", &a);
+  printf("\nEnter side b : ");
+  scanf("%f", &b);
+  printf("\nEnter side c : ");
+  scanf("%f", &c);
+  s=(a+b+c)/2.0;
+  area = sqrt(s*(s-a)*(s-b)*(s-c));
+  printf("Area of triangle is %f ", area);
+  return 0;
 }
