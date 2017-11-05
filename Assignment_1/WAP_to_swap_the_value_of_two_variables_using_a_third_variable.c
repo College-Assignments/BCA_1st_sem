3b417a98f3525afc4ae03d9227ba87bcc55f309a
Suraj Mandal <surajmandalcell@users.noreply.github.com>
2017-11-05 23:12:14 +0530

    program written

--- Assignment_1/WAP_to_swap_the_value_of_two_variables_using_a_third_variable.c
+++ Assignment_1/WAP_to_swap_the_value_of_two_variables_using_a_third_variable.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
-
-  return 0;
+int main()
+{
+    int a,b,tmp;
+    printf("Enter two Variables \n"); 
+    printf("\n\ta = ");
+    scanf("%d",&a);
+    printf("\n\tb = ");
+    scanf("%d",&b);
+    tmp=a;
+    a=b;
+    b=tmp;
+    printf("\n\ta = %d\tb = %d ", a, b);
+    return 0;
 }
+// run it here online https://repl.it/NmHr/1
